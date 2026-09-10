2e5a6c95d252b1c8e7bab76f9d62fd396f0df547
Harshit Pande <harshitpande3004@gmail.com>
2026-09-10 07:59:38 +0530

    Advanced Python - Decorators

--- Py/Day-25/Decorators.py
+++ Py/Day-25/Decorators.py
@@ -1,6 +1,5 @@
 '''Decorators'''
 
-
 def my_dec(func):
     def wrapper():
         print("Before the function is called.")
@@ -12,3 +11,27 @@ def greet():
     print("Hello Harshit!")
 greet = my_dec(greet)
 greet()
+
+
+def my_decor(func):
+    def wrapper():
+        print("Before the function is called.")
+        func()
+        print("After the function is called.")
+    return wrapper  
+@my_decor
+def greet():
+    print("Hello Harshit!")
+greet()
+
+#DIY Exercise
+def login_required(func):
+    def wrapper():
+        print("Checking login...")
+        func()
+    return wrapper
+
+@login_required
+def login():
+    print("Welcome to the dashboard!")
+login()
